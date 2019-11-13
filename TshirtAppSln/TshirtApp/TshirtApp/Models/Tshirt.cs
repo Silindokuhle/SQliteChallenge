2aed98dbdb4be93843feca1179d05076aff69323
Silindokuhle <3366171@myuwc.ac.za>
2019-11-13 15:33:37 +0200

    my new changes

---
+++ TshirtAppSln/TshirtApp/TshirtApp/Models/Tshirt.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace TshirtApp.Models
+{
+    class Tees
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }   
+        public string T_shirtsize { get; set; }
+        public string T_shirtcolor { get; set; }
+        public string Dateoforder { get; set; }
+        public string shippingaddress{ get; set; }
+    }
+}
---
+++ TshirtAppSln/TshirtApp/TshirtApp/Views/ShirtListPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TshirtApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShirtListPage : ContentPage
+    {
+        public ShirtListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
